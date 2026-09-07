aa4e9ec73190d74e2a78fa5a3a529ac132859f1a
Thomas Derham <>
2026-09-06 18:54:03 -0700

    Bump version to 0.1.1

--- Scripts/build_app_bundle.sh
+++ Scripts/build_app_bundle.sh
@@ -73,9 +73,9 @@ cat > "$APP_BUNDLE/Contents/Info.plist" <<PLIST
     <key>CFBundlePackageType</key>
     <string>APPL</string>
     <key>CFBundleShortVersionString</key>
-    <string>0.1</string>
+    <string>0.1.1</string>
     <key>CFBundleVersion</key>
-    <string>1</string>
+    <string>0.1.1</string>
     <key>NSHumanReadableCopyright</key>
     <string>© 2026 Thomas Derham</string>
     <key>LSMinimumSystemVersion</key>
